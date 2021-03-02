68cb4db38646f787bda30453a75aa11854e9f024
Hongsik Eom <haramang76@gmail.com>
2021-03-01 21:53:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ mongoose.connect(DB, {
 .catch((err) => console.log('DB connection failed'));
 
 
-// const port = process.env.PORT || 3000;
-const port = 3000;
+const port = process.env.PORT || 3000;
+//const port = 3000;
 
 app.listen(port, function () {
     console.log("Server started on port 3000");
